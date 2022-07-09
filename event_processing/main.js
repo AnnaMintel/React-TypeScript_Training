283b6fcd3803e885b7df4363e2bbbb95a0b3d788
AnnaMintel <annikamintel99@yandex.ru>
2022-07-09 11:43:01 +0300

    init: created new project and wrote html code

---
+++ event_processing/main.js
@@ -0,0 +1,6 @@
+ // event
+ // function - подписчик, слушатель, обработчик (subscriber, listener, handler)
+ // Сгенерированный браузером объект особого типа -> e, ev, event
+
+
+ const small = document.getElementById("small")
